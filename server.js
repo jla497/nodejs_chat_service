b75b5650b3eabfde862056c5129be89a0dc11e11
jla497 <jla497@sfu.ca>
2018-03-06 17:17:50 -0800

    username based login done

--- server.js
+++ server.js
@@ -1,20 +1,41 @@
 var httpclient = require('https');
-var express = require('express');
-var app = require('express')();
-var http = require('http').Server(app);
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
 var morgan = require('morgan');
-var io = require('socket.io')(http);
+var _ = require('underscore');
+var http = require('http')
+  , path = require('path')
+  , express = require('express')
+  , bodyParser = require('body-parser')
+  , cookieParser = require('cookie-parser')
+  , methodOverride = require('method-override')
+  , expressSession = require('express-session')
+  , app = express();
+
+  var myCookieParser = cookieParser('secret');
+var sessionStore = new expressSession.MemoryStore();
+//log info about requests
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(methodOverride());
+app.use(myCookieParser);
+app.use(expressSession({ 
+    secret: 'secret', 
+    store: sessionStore, 
+    resave: false,
+    saveUninitialized: false 
+}));
+
+var server = http.Server(app)
+  , io = require('socket.io')(server);
+
+var SessionSockets = require('session.socket.io')
+  , sessionSockets = new SessionSockets(io, sessionStore, myCookieParser);
+
 
 //fetch list of users from firebase
 var users = [];
   
 var req = httpclient.get('https://backbone-demo-a094e.firebaseio.com/users.json?', function(res) {
-  console.log('STATUS: ' + res.statusCode);
-  console.log('HEADERS: ' + JSON.stringify(res.headers));
-
   // Buffer the body entirely for processing as a whole.
   var bodyChunks = [];
   var jsonObj = {};
@@ -24,33 +45,12 @@ var req = httpclient.get('https://backbone-demo-a094e.firebaseio.com/users.json?
   }).on('end', function() {
     var body = Buffer.concat(bodyChunks);
     jsonObj = JSON.parse(body);
-    
-    for (var key in jsonObj) {
-        if (users.hasOwnProperty(jsonObj)) {
-            users.push_back(users[key]);
-        }
-    }
+    users = _.map(jsonObj, function(value,index) {
+        return value;
+    });
+
   })
 });
-
-
-//log info about requests
-app.use(morgan('dev'));
-
-// initialize body-parser to parse incoming parameters requests to req.body
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// initialize express-session to allow us track the logged-in user across sessions.
-app.use(session({
-    key: 'user_sid',
-    secret: 'somerandonstuffs',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        expires: 600000
-    }
-}));
-
 
 // This middleware will check if user's cookie is still saved in browser and user is not set, then automatically log the user out.
 // This usually happens when you stop your express server after login, your cookie still remains saved in the browser.
@@ -72,13 +72,13 @@ app.route('/login')
     .get(sessionChecker, (req, res) => {
         res.sendFile(__dirname + '/public/login.html');
     })
-    .post((req, res) => {
+    .post(function(req, res){
         var username = req.body.username;
-        
-        var foundUser = users.filter(function(user) {return user.username == username; });
+        var foundUser = _.filter(users, function(user) {return user.username == username; });
 
-        if(foundUser != null) {
+        if(foundUser.length > 0) {
             req.session.user = username;
+            console.log('found user: '+req.session.user);
             res.redirect('/chat');
         }else {
             res.redirect('/login');
@@ -98,21 +98,26 @@ app.get('/users', function(req, res) {
     res.sendFile(__dirname + '/public/usersApp.html');
 });
 
-
-app.use(express.static('public'));
-
-io.on('connection', function(socket) {
+sessionSockets.on('connection', function(err, socket, session) {
 
     //new connection broadcast
-    io.emit('chat message', 'new user joined the chat room');
-
+    if(session) {
+        if(session.user){ io.emit('chat message', session.user+' joined the chat room');    
+        }
+      }
+    
     //public chat event
     socket.on('chat message', function(msg) {
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
+        if(session) {
+            if(session.user){
+                io.emit('chat message', session.user+": "+msg);       
+            }
+        }
     });
 });
 
-http.listen(3000, function() {
+app.use(express.static('public'));
+
+server.listen(3000, function() {
     console.log('listening on *:3000');
 });
